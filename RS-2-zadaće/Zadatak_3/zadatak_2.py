41078e1d7518e748830db44e4b0ec345c02f7957
sasavlak <sasa.vlaketic@gmail.com>
2024-11-22 13:52:08 +0100

    Create zadatak_2.py

---
+++ RS-2-zadaće/Zadatak_3/zadatak_2.py
@@ -0,0 +1,6 @@
+rijeci = ["jabuka", "pas", "knjiga", "zvijezda", "prijatelj", "zvuk", "čokolada", "ples",
+          "pjesma", "otorinolaringolog"]
+
+duljine_sa_slovom_a = [len(rijec) for rijec in rijeci if 'a' in rijec]
+
+print(duljine_sa_slovom_a)
