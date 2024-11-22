5233d7dec013329b838c7a0817d37a4ab0b64318
sasavlak <sasa.vlaketic@gmail.com>
2024-11-22 11:40:31 +0100

    Create zadatak_4.py

---
+++ RS-2-zadaće/Zadatak_2/zadatak_4.py
@@ -0,0 +1,12 @@
+studenti = [
+    {"ime": "Ivan", "prezime": "Ivić", "godine": 19},
+    {"ime": "Marko", "prezime": "Marković", "godine": 22},
+    {"ime": "Ana", "prezime": "Anić", "godine": 21},
+    {"ime": "Petra", "prezime": "Petrić", "godine": 13},
+    {"ime": "Iva", "prezime": "Ivić", "godine": 17},
+    {"ime": "Mate", "prezime": "Matić", "godine": 18}
+]
+
+svi_punoljetni = all(map(lambda student: student["godine"] >= 18, studenti))
+
+print(svi_punoljetni)
