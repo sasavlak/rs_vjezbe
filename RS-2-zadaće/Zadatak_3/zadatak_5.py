2b630e06a85bb705d666101ae68256822a561503
sasavlak <sasa.vlaketic@gmail.com>
2024-11-22 13:52:23 +0100

    Create zadatak_5.py

---
+++ RS-2-zadaće/Zadatak_3/zadatak_5.py
@@ -0,0 +1,12 @@
+studenti = [
+    {"ime": "Ivan", "prezime": "Ivić", "bodovi": [12, 23, 53, 64]},
+    {"ime": "Marko", "prezime": "Marković", "bodovi": [33, 15, 34, 45]},
+    {"ime": "Ana", "prezime": "Anić", "bodovi": [8, 9, 4, 23, 11]},
+    {"ime": "Petra", "prezime": "Petrić", "bodovi": [87, 56, 77, 44, 98]},
+    {"ime": "Iva", "prezime": "Ivić", "bodovi": [23, 45, 67, 89, 12]},
+    {"ime": "Mate", "prezime": "Matić", "bodovi": [75, 34, 56, 78, 23]}
+]
+
+zbrojeni_bodovi = [{student["prezime"]: sum(student["bodovi"])} for student in studenti]
+
+print(zbrojeni_bodovi)
