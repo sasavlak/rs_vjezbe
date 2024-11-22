7a4f7274b12002e44716937d0733759f076cfe9d
sasavlak <sasa.vlaketic@gmail.com>
2024-11-22 11:25:53 +0100

    Update zadatak_2.py

--- RS-1-zadaće/zadatak_2.py
+++ RS-1-zadaće/zadatak_2.py
@@ -4,3 +4,4 @@ if (godina % 4 == 0 and godina % 100 != 0) or (godina % 400 == 0):
     print(f"Godina {godina}. je prijestupna godina.")
 else:
     print(f"Godina {godina}. nije prijestupna godina.")
+    
