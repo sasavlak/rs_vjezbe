421a43b7e30432751943272e2a7f178350ac2f5a
sasavlak <sasa.vlaketic@gmail.com>
2024-11-23 11:49:28 +0100

    Create zadatak_3.py

---
+++ RS-2-zadaće/Zadatak_4/zadatak_3.py
@@ -0,0 +1,23 @@
+class Student:
+    def __init__(self, ime, prezime, godine, ocjene):
+        self.ime = ime
+        self.prezime = prezime
+        self.godine = godine
+        self.ocjene = ocjene
+
+    def prosjek(self):
+        return sum(self.ocjene) / len(self.ocjene)
+
+studenti = [
+    {"ime": "Ivan", "prezime": "Ivić", "godine": 19, "ocjene": [5, 4, 3, 5, 2]},
+    {"ime": "Marko", "prezime": "Marković", "godine": 22, "ocjene": [3, 4, 5, 2, 3]},
+    {"ime": "Ana", "prezime": "Anić", "godine": 21, "ocjene": [5, 5, 5, 5, 5]},
+    {"ime": "Petra", "prezime": "Petrić", "godine": 13, "ocjene": [2, 3, 2, 4, 3]},
+    {"ime": "Iva", "prezime": "Ivić", "godine": 17, "ocjene": [4, 4, 4, 3, 5]},
+    {"ime": "Mate", "prezime": "Matić", "godine": 18, "ocjene": [5, 5, 5, 5, 5]}
+]
+
+studenti_objekti = [Student(**student) for student in studenti]
+
+najbolji_student = max(studenti_objekti, key=lambda s: s.prosjek())
+print(f"Najbolji student je {najbolji_student.ime} {najbolji_student.prezime} s prosjekom {najbolji_student.prosjek():.2f}.")
